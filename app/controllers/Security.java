3ed14c0df387f57f126c546df93b8472877944d0
Elliot Kramer <ejameskramer@gmail.com>
2012-02-23 02:30:33 -0600

    Added simple fix to only let valid users login.  Should fix the bug where users get stuck if they enter in invalid info

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -6,4 +6,11 @@ public class Security extends Secure.Security {
     public static String connected() {
         return Secure.Security.connected();
     }
+    
+    public static boolean authenticate(String username, String password) {
+    	if (User.connect(username, password) == null) {
+    		return false;
+    	}
+    	return true;
+    }
 }
