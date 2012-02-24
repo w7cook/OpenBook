70ddda756bb28f1ad7a28bacfcb15df6ab0a0033
Elliot Kramer <ejameskramer@gmail.com>
2012-02-24 17:23:06 -0600

    Changed indentation in all affected files to the new 2 space standard

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,177 +2,177 @@ package controllers;
 
 import java.util.*;
 
-import play.*;
-import play.mvc.*;
-import controllers.Secure;
 import models.*;
 
 @With(Secure.class)
 public class Application extends Controller {
 
-	@Before
-	static void addDefaults() {
-	}
+  @Before
+  static void addDefaults() {
+  }
 
-	public static User user() {
-		assert Secure.Security.connected() != null;
-		return User.find("byEmail", Secure.Security.connected()).first();
-	}
+  public static User user() {
+    assert Secure.Security.connected() != null;
+    return User.find("byEmail", Secure.Security.connected()).first();
+  }
 
-	public static void about(Long id) {
-		User user = id == null ? user() : (User) User.findById(id);
-		render(user);
-	}
+  public static void about(Long id) {
+    User user = id == null ? user() : (User) User.findById(id);
+    render(user);
+  }
 
-	public static void news(Long id) {
-		User user = id == null ? user() : (User) User.findById(id);
-		render(user);
-	}
+  public static void news(Long id) {
+    User user = id == null ? user() : (User) User.findById(id);
+    render(user);
+  }
 
-	public static void account() {
-		User user = user();
-		render(user);
-	}
+  public static void account() {
+    User user = user();
+    render(user);
+  }
 
-	private static boolean given(String val) {
-		return val != null && val.length() > 0;
-	}
-	
-	
-	/** Request to be friends with a given user, changing appropriate Relationship flags where necessary. 
-	 * 
-	 * @param id the user to request friendship with
-	 */
-	public static void requestFriends(Long id) {
-		User user = user();
-		User other = User.findById(id);
-		Relationship r1 = Relationship.find("SELECT r FROM Relationship r where r.from = ? AND r.to = ?", user, other).first();
-		Relationship r2 = Relationship.find("SELECT r FROM Relationship r where r.to = ? AND r.from = ?", user, other).first();
+  private static boolean given(String val) {
+    return val != null && val.length() > 0;
+  }
 
-		if (r1 == null) {
-			r1 = new Relationship(user, other, true);
-		}
-		
-		if (r2 != null) {
-			
-			// If the other user has already requested, this request should make them friends
-			if (r2.requested) {
-				r1.requested = false;
-				r2.requested = false;
-				r1.accepted = true;
-				r2.accepted = true;
-				r1.save();
-				r2.save();
-			} else if (r2.accepted){
-				news(id);
-				return;
-			} else {
-				r1.requested = true;
-			}
-		}
-		// If the user has already made a request for friendship, do nothing
-		else if (r1 != null) {
-			if (r1.requested) {
-				news(id);
-				return;
-			} else {
-				r1.requested = true;
-				r1.save();
-				news(id);
-				return;
-			}
-		}
-		r1.save();
-		news(id);
-	}
-	
-	/** Attempt to end a relationship with a user, changing appropriate Relationship flags where necessary
-	 * 
-	 * @param id the user to remove
-	 */
-	public static void removeFriends(Long id) {
-		User user = user();
-		User other = User.findById(id);
-		Relationship r1 = Relationship.find("SELECT r FROM Relationship r where r.from = ? AND r.to = ?", user, other).first();
-		Relationship r2 = Relationship.find("SELECT r FROM Relationship r where r.to = ? AND r.from = ?", user, other).first();
-		
-		if (r1 != null) { 
-			r1.requested = false;
-			r1.accepted = false;
-			r1.save();
-		}
-		if (r2 != null) {
-			r2.accepted = false;
-			r2.requested = false;
-			r2.save();
-		}
-		news(id);
-	}
 
-	public static void account_save(User update, String old_password) {
-		User currentUser = user();
+  /** Request to be friends with a given user, changing appropriate Relationship flags where necessary.
+   * 
+   * @param id the user to request friendship with
+   */
+  public static void requestFriends(Long id) {
+    User user = user();
+    User other = User.findById(id);
+    Relationship r1 = Relationship.find("SELECT r FROM Relationship r where r.from = ? AND r.to = ?", user, other).first();
+    Relationship r2 = Relationship.find("SELECT r FROM Relationship r where r.to = ? AND r.from = ?", user, other).first();
 
-		validation.required(update.first_name).message("First name is required");
-		validation.required(update.username).message("Username is required");
-		validation.required(update.email).message("Email is required");
-		validation.isTrue(currentUser.password.equals(old_password)).message(
-				"Password does not match");
-		if (validation.hasErrors()) {
-			User user = update;
-			renderTemplate("Application/account.html", user);
-		} else {
-			User user = currentUser;
-			String name = "";
-			if (given(update.first_name)) {
-				user.first_name = update.first_name;
-				name += user.first_name;
-			}
-			if (given(update.middle_name)) {
-				user.middle_name = update.middle_name;
-				if (given(name))
-					name += " ";
-				name += user.first_name;
-			}
-			if (given(update.last_name)) {
-				user.last_name = update.last_name;
-				if (given(name))
-					name += " ";
-				name += user.first_name;
-			}
-			user.name = name;
-			user.username = update.username;
-			user.email = update.email;
-			if (given(update.password))
-				user.password = update.password;
-			user.save();
-			account();
-		}
-	}
+    if (r1 == null) {
+      r1 = new Relationship(user, other, true);
+    }
 
-	public static void edit_basic() {
-		long userID = 1;
-		User user = User.findById(userID);
-		render(user);
-	}
+    if (r2 != null) {
 
-	public static void updateBasic() {
-		long userID = 1;
-		User user = User.findById(userID);
-		render(user);
-	}
+      // If the other user has already requested, this request should make them friends
+      if (r2.requested) {
+        r1.requested = false;
+        r2.requested = false;
+        r1.accepted = true;
+        r2.accepted = true;
+        r1.save();
+        r2.save();
+      } else if (r2.accepted){
+        news(id);
+        return;
+      } else {
+        r1.requested = true;
+      }
+    }
+    // If the user has already made a request for friendship, do nothing
+    else if (r1 != null) {
+      if (r1.requested) {
+        news(id);
+        return;
+      } else {
+        r1.requested = true;
+        r1.save();
+        news(id);
+        return;
+      }
+    }
+    r1.save();
+    news(id);
+  }
 
-	public static void search(String query) {
-		// not implemented yet
-	}
+  /** Attempt to end a relationship with a user, changing appropriate Relationship flags where necessary
+   * 
+   * @param id the user to remove
+   */
+  public static void removeFriends(Long id) {
+    User user = user();
+    User other = User.findById(id);
+    Relationship r1 = Relationship.find("SELECT r FROM Relationship r where r.from = ? AND r.to = ?", user, other).first();
+    Relationship r2 = Relationship.find("SELECT r FROM Relationship r where r.to = ? AND r.from = ?", user, other).first();
 
-	public static void deleteComment(Long id, Long page) {
-		Comment c = Comment.findById(id);
-		c.delete();
-		news(page);
-	}
+    if (r1 != null) {
+      r1.requested = false;
+      r1.accepted = false;
+      r1.save();
+    }
+    if (r2 != null) {
+      r2.accepted = false;
+      r2.requested = false;
+      r2.save();
+    }
+    news(id);
+  }
 
-	public static void postComment(Long postId, String author, String content) {
-		Post post = Post.findById(postId);
-		post.addComment(author, content);
-	}
+  public static void account_save(User update, String old_password) {
+    User currentUser = user();
+
+    validation.required(update.first_name).message("First name is required");
+    validation.required(update.username).message("Username is required");
+    validation.required(update.email).message("Email is required");
+    validation.isTrue(currentUser.password.equals(old_password)).message(
+    "Password does not match");
+    if (validation.hasErrors()) {
+      User user = update;
+      renderTemplate("Application/account.html", user);
+    } else {
+      User user = currentUser;
+      String name = "";
+      if (given(update.first_name)) {
+        user.first_name = update.first_name;
+        name += user.first_name;
+      }
+      if (given(update.middle_name)) {
+        user.middle_name = update.middle_name;
+        if (given(name)) {
+          name += " ";
+        }
+        name += user.first_name;
+      }
+      if (given(update.last_name)) {
+        user.last_name = update.last_name;
+        if (given(name)) {
+          name += " ";
+        }
+        name += user.first_name;
+      }
+      user.name = name;
+      user.username = update.username;
+      user.email = update.email;
+      if (given(update.password)) {
+        user.password = update.password;
+      }
+      user.save();
+      account();
+    }
+  }
+
+  public static void edit_basic() {
+    long userID = 1;
+    User user = User.findById(userID);
+    render(user);
+  }
+
+  public static void updateBasic() {
+    long userID = 1;
+    User user = User.findById(userID);
+    render(user);
+  }
+
+  public static void search(String query) {
+    // not implemented yet
+  }
+
+  public static void deleteComment(Long id, Long page) {
+    Comment c = Comment.findById(id);
+    c.delete();
+    news(page);
+  }
+
+  public static void postComment(Long postId, String author, String content) {
+    Post post = Post.findById(postId);
+    post.addComment(author, content);
+  }
 }
--- app/models/User.java
+++ app/models/User.java
@@ -1,133 +1,129 @@
 package models;
 
 import java.util.*;
-import javax.persistence.*;
-
 import controllers.Application;
-
-import play.db.jpa.*;
 
 @Entity
 public class User extends Model {
 
-	public String name; // The user's full name
-	public String first_name; // The user's first name
-	public String middle_name; // The user's middle name
-	public String last_name; // The user's last name
-	public String gender; // The user's gender: female or male
-	public String locale; // The user's locale (ISO Language Code and ISO Country
-												// Code)
-	
-	public String username; // The user's username
-	public float timezone; // The user's timezone offset from UTC
-	public Date updated_time; // The last time the user's profile was updated;
-														// changes to the
-														// languages, link, timezone, verified,
-														// interested_in, favorite_athletes, favorite_teams,
-														// andvideo_upload_limits are not not reflected in
-														// this value
-	
-	public boolean verified; // The user's account verification status,
-														// either true or false(see below)
-	public String bio; // The user's biography
-	public Date birthday; // The user's birthday
-	// @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
-	// public List<Enrollment> education; // A list of the user's education
-	// history
-	public String email; // The proxied or contact email address granted by the
-												// user
+  public String name; // The user's full name
+  public String first_name; // The user's first name
+  public String middle_name; // The user's middle name
+  public String last_name; // The user's last name
+  public String gender; // The user's gender: female or male
+  public String locale; // The user's locale (ISO Language Code and ISO Country
+  // Code)
 
-	@OneToOne
-	public User significant_other; // The user's significant other
-	public Date anniversary; // date of anniversary
-	public String website; // The URL of the user's personal website
-	
-	// not implemented yet!!
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-	public List<UserLanguage> languages; // The user's languages
-	@ManyToOne
-	public Location hometown; // The user's hometown
-	// public List<String> interested_in; // The genders the user is interested in
-	@ManyToOne
-	public Location location; // The user's current city
+  public String username; // The user's username
+  public float timezone; // The user's timezone offset from UTC
+  public Date updated_time; // The last time the user's profile was updated;
+  // changes to the
+  // languages, link, timezone, verified,
+  // interested_in, favorite_athletes, favorite_teams,
+  // andvideo_upload_limits are not not reflected in
+  // this value
 
-	public String political; // The user's political view
-	public String quotes; // The user's favorite quotes
-	public String relationship_status; // The user's relationship
-																			// status:Single, In a
-																			// relationship, Engaged,Married, It's
-																			// complicated, In an open
-																			// relationship, Widowed,Separated, Divorced, In
-																			// a civil union, In a domestic
-																			// partnership
-	public String religion; // The user's religion
+  public boolean verified; // The user's account verification status,
+  // either true or false(see below)
+  public String bio; // The user's biography
+  public Date birthday; // The user's birthday
+  // @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
+  // public List<Enrollment> education; // A list of the user's education
+  // history
+  public String email; // The proxied or contact email address granted by the
+  // user
 
-	@OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
-	public List<Employment> work; // A list of the user's work history
+  @OneToOne
+  public User significant_other; // The user's significant other
+  public Date anniversary; // date of anniversary
+  public String website; // The URL of the user's personal website
 
-	public String password;
+  // not implemented yet!!
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+  public List<UserLanguage> languages; // The user's languages
+  @ManyToOne
+  public Location hometown; // The user's hometown
+  // public List<String> interested_in; // The genders the user is interested in
+  @ManyToOne
+  public Location location; // The user's current city
 
-	@OneToMany(mappedBy = "from", cascade = CascadeType.ALL)
-	public List<Relationship> friends; // A list of the user's work history
+  public String political; // The user's political view
+  public String quotes; // The user's favorite quotes
+  public String relationship_status; // The user's relationship
+  // status:Single, In a
+  // relationship, Engaged,Married, It's
+  // complicated, In an open
+  // relationship, Widowed,Separated, Divorced, In
+  // a civil union, In a domestic
+  // partnership
+  public String religion; // The user's religion
 
-	@OneToMany(mappedBy = "to", cascade = CascadeType.ALL)
-	public List<Relationship> friendedBy; // A list of the user's work history
+  @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
+  public List<Employment> work; // A list of the user's work history
 
-	public User(String email, String password, String username) {
-		this.email = email;
-		this.password = password;
-		this.username = username;
-		// this.education = new ArrayList<Enrollment>();
-	}
+  public String password;
 
-	public static User connect(String email, String password) {
-		return find("byEmailAndPassword", email, password).first();
-	}
+  @OneToMany(mappedBy = "from", cascade = CascadeType.ALL)
+  public List<Relationship> friends; // A list of the user's work history
 
-	public List<Post> news() {
-		return Post.find(
-				"SELECT p FROM Post p, IN(p.author.friendedBy) u WHERE u.from.id = ? and (U.accepted = true or u.to.id = ?)",
-				this.id, this.id).fetch();
-	}
-	
-	/** Checks the status of a friendship
-	 * 
-	 * @param id the user to check friendship status with
-	 * @return a string representing the status
-	 */
-	public String checkFriendship(Long id) {
-		User current = Application.user();
-		if (Application.user().id == id) {
-			return "";
-		}
-		Relationship r1 = Relationship.find("SELECT r FROM Relationship r where r.from = ? AND r.to = ?", current, this).first();
-		Relationship r2 = Relationship.find("SELECT r FROM Relationship r where r.to = ? AND r.from = ?", current, this).first();
-		if (r1 != null) {
-			if (r1.accepted) {
-				return "Friends";
-			}
-			if (r1.requested){
-				return "Friendship Requested";
-			}
-		}
-		return "Request Friendship";
-	}
-	
-	/** Get any confirmed friends
-	 * 
-	 * @return a list of relationships for confirmed friends
-	 */
-	public List<Relationship> confirmedFriends() {
-		return Relationship.find("SELECT r FROM Relationship r where r.from = ? and r.accepted = true", this).fetch();
-	}
-	
-	
-	/** Get a list of any users who have requested to be friends
-	 * 
-	 * @return a list of relationships related to incoming friend requests
-	 */
-	public List<Relationship> requestedFriends() {
-		return Relationship.find("SELECT r FROM Relationship r where r.to = ? and r.requested = true and r.accepted = false", this).fetch();
-	}
+  @OneToMany(mappedBy = "to", cascade = CascadeType.ALL)
+  public List<Relationship> friendedBy; // A list of the user's work history
+
+  public User(String email, String password, String username) {
+    this.email = email;
+    this.password = password;
+    this.username = username;
+    // this.education = new ArrayList<Enrollment>();
+  }
+
+  public static User connect(String email, String password) {
+    return find("byEmailAndPassword", email, password).first();
+  }
+
+  public List<Post> news() {
+    return Post.find(
+        "SELECT p FROM Post p, IN(p.author.friendedBy) u WHERE u.from.id = ? and (U.accepted = true or u.to.id = ?)",
+        this.id, this.id).fetch();
+  }
+
+  /** Checks the status of a friendship
+   * 
+   * @param id the user to check friendship status with
+   * @return a string representing the status
+   */
+  public String checkFriendship(Long id) {
+    User current = Application.user();
+    if (Application.user().id == id) {
+      return "";
+    }
+    Relationship r1 = Relationship.find("SELECT r FROM Relationship r where r.from = ? AND r.to = ?", current, this).first();
+    Relationship r2 = Relationship.find("SELECT r FROM Relationship r where r.to = ? AND r.from = ?", current, this).first();
+    if (r1 != null) {
+      if (r1.accepted) {
+        return "Friends";
+      }
+      if (r1.requested){
+        return "Friendship Requested";
+      }
+    }
+    return "Request Friendship";
+  }
+
+  /** Get any confirmed friends
+   * 
+   * @return a list of relationships for confirmed friends
+   */
+  public List<Relationship> confirmedFriends() {
+    return Relationship.find("SELECT r FROM Relationship r where r.from = ? and r.accepted = true", this).fetch();
+  }
+
+
+  /** Get a list of any users who have requested to be friends
+   * 
+   * @return a list of relationships related to incoming friend requests
+   */
+  public List<Relationship> requestedFriends() {
+    return Relationship.find("SELECT r FROM Relationship r where r.to = ? and r.requested = true and r.accepted = false", this).fetch();
+  }
 
 }
