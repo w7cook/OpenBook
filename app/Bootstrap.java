f2eeb8052dd210f00ab33a2889c89b6fd9606a2c
Geoffrey Parker <professional.cynic@gmail.com>
2012-03-02 17:08:07 -0600

    Added password hashing.

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,17 +1,25 @@
 import play.*;
 import play.jobs.*;
 import play.test.*;
- 
+import play.libs.Crypto;
+
 import models.*;
- 
+
+import java.util.*;
+
 @OnApplicationStart
 public class Bootstrap extends Job {
- 
+
     public void doJob() {
         // Check if the database is empty
         if(User.count() == 0) {
             Fixtures.loadModels("initial-data.yml");
+            List<User> users= User.findAll();
+            for(User u : users) {
+              u.password = Crypto.passwordHash(u.password);
+              u.save();
+            }
         }
     }
- 
+
 }
