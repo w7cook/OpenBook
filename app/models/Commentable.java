95fabcf66cc77842e85366fd387e1da53b1a0c60
Taylor Keeler <tkeeler.cs@gmail.com>
2012-03-03 22:10:00 -0600

    Pushed working files

--- app/models/Commentable.java
+++ app/models/Commentable.java
@@ -13,10 +13,22 @@ public class Commentable extends Model
 	@OneToMany(mappedBy = "parentObj", cascade = CascadeType.ALL)
 	public List<Comment> allComments;
 	
+	// @OneToMany(mappedBy = "parentObj", cascade = CascadeType.ALL)
+	// public List<Like> allLikes;
+	
 	public Commentable addComment(String author, String content) {
 		Comment newComment = new Comment(this, author, content).save();
 		this.allComments.add(newComment);
 		this.save();
 		return this;
 	}
+	
+	/*
+	public Commentable addLike( ){
+	  Like newLike = new Like( ).save();
+	  this.addLike.add(newLike);
+	  this.save();
+	  return this;
+	}
+	*/
 }
--- app/models/Status.java
+++ app/models/Status.java
@@ -2,18 +2,20 @@ package models;
 
 import java.util.*;
 import javax.persistence.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 import play.db.jpa.*;
 import play.data.validation.*;
 
 @Entity
-public class Status extends Model {
+public class Status extends Commentable {
 /*
 * The reason status messages did not replace post is because post can still be
 * used as a blog post for a user. The idea is to use status messages as quick
 * news message.
 */
-
+  private static final Pattern links_pattern = Pattern.compile("\\b?[@#]\\w*\\b"); // A pattern to pull links (mentions and tags) from content
  
   @Required
   @ManyToOne
@@ -24,72 +26,49 @@ public class Status extends Model {
   @MaxSize(1000)
   public String content; // The status update text
   
-  private String unlinked_content;
-  
   public Date date; // The time when submitted
   
-  // ############## TO BE IMPLEMENTED #######################################
+  public Object linked_object;  // Think Retweet
   
-  // public Status linked_status;  // Think Retweet
+  @ManyToMany(cascade=CascadeType.PERSIST)
+  public Set<Tag> tags;
   
-  // public Post linked_post;
-  
-  // @Required
-  // public String type; // Text, Link, Location(Check in), Poll
-  
-  // @OneToMany(mappedBy = "status", cascade = CascadeType.ALL)
-  // public List<Comment> comments;
-  
-  // @OneToMany(mappedBy="status", cascade=CascadeType.ALL)
-  // public List<Like> likes;
-  
-  // @ManyToMany(cascade=CascadeType.PERSIST)
-  // public Set<Tag> tags;
-  
-  // @ManyToMany(cascade=CascadeType.PERSIST)
-  //public List<User> mentions;
-  
-  // ########################################################################
+  @ManyToMany
+  public List<User> mentions;
   
   public Status(User author, String content) {
-    // this.comments = new ArrayList<Comment>();
-    // this.likes = new ArrayList<Like>();
-    // this.tags = new ArrayList<Tag>();
+    this.tags = new ArrayList<Tag>();
+    this.mentions = new ArrayList<User>();
     this.author = author;
-    this.unlinked_content = content;
-    this.content = parseContent(unlinked_content);
+    this.content = parseContent(content);
     this.date = new Date();
-    // this.mentions = someFunction(...message parsing...);
-    // this.linked_post = someFunction(...message parsing...);
-    // this.linked_status = someFuntion(...message parsing...);
+    this.linked_object = someFunction(...message parsing...);
   }
 
   private String parseContent(String unlinked_content){
     // TODO implement a string parser that pulls out @ and # tags
+    Matcher links_matcher = links_pattern.matcher(unlinked_content);
+    
+    while(links_matcher.find() ){
+      String match = links_matcher.group();
+      if(match.startsWith("#") { // tag
+        Tag newTag = 
+        this.tags.add(newTag);
+        this.save();
+      }
+      else if(match.startsWith("@") {
+        
+      }
+      else
+      // error
+    }
+    
     return unlinked_content;
   }
   
   /* TODO
   private static someFunction(...message parsing...){
     
-  }
-  */
-  
-  /* TODO
-  public Status addComment(String author, String content){
-    Comment newComment = new Comment(this, author, content).save();
-    this.comments.add(newComment);
-    this.save();
-    return this;
-  }
-  */
-  
-  /* TODO
-  public Status addLike(...){
-    Like newLike = new Like(...).save();
-    this.likes.add(newLike);
-    this.save();
-    return this;
   }
   */
 
@@ -100,12 +79,6 @@ public class Status extends Model {
   public Status next() {
     return Post.find("update_time > ? order by update_time asc", date).first();
   }
-  
-  /* TODO
-  public List<Comment> comments() {
-    return comments;
-  }
-  */
   
   /* TODO
   public static List<Status> findTaggedWith(String... tags) {
