653a7e0ec0e22320b55fe60ce45b448df0c54b1f
Aaron Stolarz <ars2676@cs.utexas.edu>
2012-03-21 11:17:35 -0500

    Added empty 'change picture' link to profile image

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -13,6 +13,7 @@ public class Bootstrap extends Job {
   public void doJob() {
     // Check if the database is empty
     if(User.count() == 0) {
+      Fixtures.loadModels("default-data.yml");
       Fixtures.loadModels("initial-data.yml");
       List<User> users= User.findAll();
       for(User u : users) {
