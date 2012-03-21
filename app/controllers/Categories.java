1fdc17fdfcea3d02df3dfd16e2c1274140493fe7
Sean Coyle <seanpcoyle@gmail.com>
2012-03-21 13:31:24 -0500

    Fixed null pointer.  Changed routes.

--- app/controllers/Categories.java
+++ app/controllers/Categories.java
@@ -12,6 +12,20 @@ import java.util.*;
 import javax.persistence.*;
 
 public class Categories extends Controller {
+	
+	@Before
+	static void setConnectedUser() 
+	{
+		if (Security.isConnected()) 
+		{
+			renderArgs.put("currentUser", user());
+		}
+	}
+	
+	public static User user() {
+		assert Secure.Security.connected() != null;
+		return User.find("byEmail", Secure.Security.connected()).first();
+	}
 
 	public static void listAll()
 	{
