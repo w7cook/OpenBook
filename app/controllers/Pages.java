d8cd01eff7dfc2194c4262c41fa58489eda01b12
Darby <darbs@HuevosRancheros.(none)>
2012-03-22 20:51:58 -0500

    Redone page structure works now with admin as user
    known issues:
      cant update pages
      UserPage links everyone as fan

--- app/controllers/Pages.java
+++ app/controllers/Pages.java
@@ -7,65 +7,45 @@ import play.mvc.*;
 import controllers.Secure;
 import models.*;
 
-
-@With(Secure.class)      
-public class Pages extends Application { 
-	//list all pages
-	public static void pages(){
-		List<Page> pageList = Page.findAll(); 
-		renderTemplate("Page/pages.html", pageList);
-	}
-	//newPage view
-  public static void newPage(){
-  	renderTemplate("Page/myPage.html");
-  }
-
-  //link newPage to Page after creating page   
-	public static void savePage(String title, String info){
+@With(Secure.class)
+public class Pages extends OBController {
+	
+	public static void newPage(){
 		User currentUser = user();
-		//remove leading and trailing whitespaces
-		info = info.replaceAll("^\\s+", "").replaceAll("\\s+$", "");
-		Page page = findMyPage(currentUser,title);
-		if(page != null){
-			page.update(info);
-			page.save();			
-			renderTemplate("Page/myPage.html", page, currentUser);
-		}
-		renderText("null page error");
+		render(currentUser);
 	}
-	//Makes a new page
-	public static void makePage(String title, String info){
-		info = info.replaceAll("^\\s+", "").replaceAll("\\s+$", "");
+	
+	public static void pageSave(String title, String info){
 		User currentUser = user();
-		Page page = new Page(currentUser, title, info);
-		UserPage mypage = new UserPage(page,currentUser);
-		page.save();
-		mypage.save();
-		renderTemplate("Page/myPage.html", page, currentUser);
-	}
-	//admin ? admin view : fan view;
-	public static void display(Page page){
-		User currentUser = user();
-		int fanFlag;
-		if((page.admin).equals(currentUser)){
-			renderTemplate("Page/myPage.html", page);
+		Page page  = Page.find("Select p from Page p where p.title = ?", title).first();
+		if(page == null){
+			page = new Page(currentUser, title, info).save();
+			UserPage pageLink = new UserPage(currentUser, page).save();
 		}
 		else{
-			renderTemplate("Page/page.html",page);
+			page.info = info;
+			page.save();
 		}
-	}                             
-	
-	public static Page findMyPage(User user, String title){
-		Page mypage = Page.find("SELECT u FROM Page u where u.title= ? AND u.admin = ?", title, user.email).first();
-		if(mypage != null){return mypage;}
-		else{return null;}
-	}
-	//Fix
-	public int isFan(Page p){
-		UserPage u = UserPage.find("Select u from UserPage u where u.fan = ? and u.page = ?", user(), p).first();
-		renderText(u.page.title);
-		if(u == null){return 0;}
-		else{return 1;}
+		//renderText(page.admin+"\n"+page.title+"\n"+page.info+"\n"+pageLink.page.title+"\n"+pageLink.fan);
+		render("Pages/myPage.html", page,currentUser);
 	}
 	
+	public static void myPages(){
+		User currentUser = user(); 
+		List<UserPage> myPages = UserPage.find("select u from UserPage u where u.fan = ?", currentUser).fetch();
+		render(myPages);
+	}
+	
+	public static void display(Long id){
+		User currentUser = user();
+		Page page = Page.findById(id);
+		//UserPage pageLink = UserPage.find("selece u from UserPage u where u.page = ?", page)
+		UserPage pageLink = UserPage.findById(id);
+		render("Pages/myPage.html", page, pageLink, currentUser);
+	}
+
+	public static void pages(){
+		List<Page> allPages = Page.findAll();
+		render(allPages);
+	}
 }
--- app/models/Page.java
+++ app/models/Page.java
@@ -3,36 +3,31 @@ package models;
 import java.util.*;
 import javax.persistence.*;
 
+import org.elasticsearch.index.query.QueryBuilders;
 import controllers.Application;
-
+import controllers.Skins;
+import controllers.Users;
 import play.db.jpa.*;
+import play.modules.elasticsearch.*;
+import play.modules.elasticsearch.annotations.ElasticSearchable;
+import play.modules.elasticsearch.search.SearchResults;
+import play.libs.Crypto;
 
+@ElasticSearchable
 @Entity
 public class Page extends Model {
-		
-	public String title;
+	
+	@OneToOne
+	public User admin;
 	
 	@Lob
 	public String info;
+	public String title;
 	
-	public ArrayList<Comment> comments;
-	
-	
-	public User admin;
-	
-	public Page(){
-		this.comments = new ArrayList<Comment>();
-	}
-	
-	public Page(User user, String name, String info){
-		this.comments = new ArrayList<Comment>();
-		this.title = name;
+	public Page(User user, String newTitle, String newInfo){
 		this.admin = user;
-		this.info = info;
+		this.title = newTitle;
+		this.info = newInfo;
 	}
-	
-	public void update(String info){
-		this.info = info;
-	}
-	
+
 }
