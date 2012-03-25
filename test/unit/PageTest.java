ae10e90e0515be50721ff4492ca4b24d3dc56251
Darby <darbs@HuevosRancheros.(none)>
2012-03-25 00:59:11 -0500

    implemented some page tests

--- test/unit/PageTest.java
+++ test/unit/PageTest.java
@@ -16,12 +16,39 @@ public class PageTest extends UnitTest {
    */
   @Test 
   public void newPageTest(){
-  	User bob = new User("bob@gmail.com", "secret", "Bob").save();
-  	assertNotNull(bob.createdAt);
-  	Page bobsPage = new Page(bob, "testPage1", "this is a test").save();
-  	assertEquals("testPage1", bobsPage.title);
-  	assertEquals("this is a test", bobsPage.info);
-  	assertEquals("bob@gmail.com", bobsPage.admin);
+  	User billy = new User("bob@gmail.com", "secret", "Billy").save();
+  	assertNotNull(billy.createdAt);
+  	Page billysPage = new Page(billy, "testPage1", "this is a test").save();
+  	assertEquals("testPage1", billysPage.title);
+  	assertEquals("this is a test", billysPage.info);
+  	assertEquals(billy, billysPage.admin);
+  	
+  	User loser = new User("loser@gmail.com", "lame", "me").save();
+  	assertNotNull(loser.createdAt);
+  	Page loserPage = new Page(loser, "HappyDays", "kittens").save();
+  	assertNotNull(loserPage);                                          
   }
   
+  @Test
+  public void newUserPageTest(){
+  	User billy = User.find("select b from User b where b.username = ?", "billy").first();
+   	User loser = User.find("select c from User c where c.username = ?", "loser").first();
+  	Page loserPage = Page.find("select p from Page p where p.admin = ?", loser).first();
+  	Page billysPage = Page.find("select p from Page p where p.admin = ?", billy).first();
+  	UserPage link1 = new UserPage(billy, loserPage).save();
+  	assertNotNull(link1);
+  	assertEquals(link1.page, loserPage);
+  	assertEquals(link1.fan, billy);
+  	
+  	UserPage link2 = new UserPage(billy, billysPage).save();
+  	assertNotNull(link2);
+  	assertEquals(link2.page, billysPage);
+  	assertEquals(link2.fan, billy);
+  }
+  
+   @Test
+  public void databasePageTest(){
+  	Page billysPage = Page.find("select p from Page p where p.title = ?", "testPage1").first();
+  	assertNotNull(billysPage);
+  }
 }
