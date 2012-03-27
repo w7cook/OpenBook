e636f63b5cd40b3c0023bbbe3481a53b7651667a
Andy Eskridge <andy.eskridge@gmail.com>
2012-03-27 00:39:41 -0500

    added routes and basic view

--- app/controllers/Notes.java
+++ app/controllers/Notes.java
@@ -1,12 +1,24 @@
 package controllers;
 
+import models.User;
 import play.mvc.*;
 
 @With(Secure.class)
-public class Notes extends Controller {
+public class Notes extends OBController {
 
-    public static void index() {
-        render();
+    public static void viewNotes() {
+    	User user = user();
+        render(user);
+    }
+    
+    public static void newNote() {
+    	User user = user();
+        render(user);
+    }
+    
+    public static void saveNote() {
+    	User user = user();
+        render(user);
     }
 
 }
