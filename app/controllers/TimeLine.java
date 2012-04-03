503858fa8930621a41edbd96735d7c38b49630c5
Taylor Keeler <tkeeler.cs@gmail.com>
2012-04-03 15:16:21 -0500

    Updated basic TimeLine structure

--- app/controllers/TimeLine.java
+++ app/controllers/TimeLine.java
@@ -1,5 +1,21 @@
-package Controllers
+package controllers;
 
-public TimeLine extends OBController {
+import java.util.*;
 
+public class  TimeLine extends OBController {
+
+  public void newsfeed(User user){
+    List<Status> timeline = Status.all();
+    render(timeline);
+  } 
+
+  public void timeline(User user){
+    render(null);
+  }
+
+  public void updateStatus(...){
+    render(null);
+  } 
+  
+  
 }
