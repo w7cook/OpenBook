d5dada9f1dc16edaebde3b9d8320246cc3d608a3
Geoffrey Parker <professional.cynic@gmail.com>
2012-04-12 22:27:32 -0500

    starting to fill out a proper api.

--- app/controllers/Posts.java
+++ app/controllers/Posts.java
@@ -8,20 +8,18 @@ import play.utils.HTML;
 import controllers.Secure;
 import models.*;
 
-@With(Secure.class)
 public class Posts extends OBController {
+  //comments(Long id): will render the user being viewed unless it is a null user then it will render the current user
+  public static void posts(Long id) {
+    User user = id == null ? user() : (User) User.findById(id);
+    render(user);
+  }
 
-	//comments(Long id): will render the user being viewed unless it is a null user then it will render the current user
-	public static void posts(Long id) {
-		User user = id == null ? user() : (User) User.findById(id);
-		render(user);
-	}
-
-	public static void deletePost(Long id, Long userId) {
-		Post p = Post.findById(id);
-		p.delete();
-		posts(userId);
-	}
+  public static void deletePost(Long id, Long userId) {
+    Post p = Post.findById(id);
+    p.delete();
+    posts(userId);
+  }
 
   public static void newPost(Long userId, String post_content) {
     new Post((User)User.findById(userId), new Date().toString(), post_content).save();
@@ -29,8 +27,8 @@ public class Posts extends OBController {
   }
 
   public static void makeNewPost(String postContent) {
-    final Post p = new Post(user(), new Date().toString(), 
-        HTML.htmlEscape(postContent)).save();
+    final Post p = new Post(user(), new Date().toString(),
+                            HTML.htmlEscape(postContent)).save();
     Map<String, Object> m = new HashMap<String, Object>();
     m.put("item", p);
     m.put("user", user());
@@ -39,9 +37,8 @@ public class Posts extends OBController {
   }
 
   public static void poke (Long userId) {
-  	String poked = new String(user() + " has poked " + (User)User.findById(userId) + "!");
-  	new Post(user(), new Date().toString(), poked).save();
+    String poked = new String(user() + " has poked " + (User)User.findById(userId) + "!");
+    new Post(user(), new Date().toString(), poked).save();
     posts(userId);
   }
 }
-
