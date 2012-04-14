89fdf62d7effd020ed95aeb24521df92089d9b95
Faraaz Ismail <fism88@gmail.com>
2012-04-13 20:57:54 -0500

    Saving all fields of edit_basic form in controller

--- app/controllers/Profiles.java
+++ app/controllers/Profiles.java
@@ -10,14 +10,25 @@ import models.*;
 @With(Secure.class)
 public class Profiles extends OBController {
   
-  public static void edit_basic(User u) {
-    render(u);
-  }
-  public static void updateBasic(Long userId, String religion) {
+  public static void updateBasic(Long userId, String religion, Date birthday, String gender,
+      String relationshipStatus, List<UserLanguage> languages, String political, String phone,
+      String address, List<Enrollment> education, List<Employment> work, Location hometown,
+      String quotes) {
     User u = User.findById(userId);
     u.profile.religion = religion;
-    u.profile.save(); 
+    u.profile.birthday = birthday;
+    u.profile.gender = gender;
+    u.profile.relationshipStatus = relationshipStatus;
+    u.profile.languages = languages;
+    u.profile.political = political;
+    u.profile.phone = phone;
+    u.profile.address = address;
+    u.profile.education = education;
+    u.profile.work = work;
+    u.profile.hometown = hometown;
+    u.profile.quotes = quotes;
+    u.profile.save();
     System.out.println("Relgion = " + religion);
-    edit_basic(u);
+    render();
   }
 }
