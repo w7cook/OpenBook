1c4d8ff6549f7c074b2e26aa31605c0bef2ad356
Faraaz Ismail <fism88@gmail.com>
2012-04-15 01:42:43 -0500

    minor cleanup

--- app/controllers/Profiles.java
+++ app/controllers/Profiles.java
@@ -14,22 +14,20 @@ public class Profiles extends OBController {
       String relationshipStatus, List<UserLanguage> languages, String political, String phone,
       String address, List<Enrollment> education, List<Employment> work, Location hometown,
       String quotes) {
-    User u = user;
-    u.profile.religion = religion;
-    u.profile.birthday = birthday;
-    u.profile.gender = gender;
-    u.profile.relationshipStatus = "Single";
-    u.profile.languages = languages;
-    u.profile.political = political;
-    u.profile.phone = phone;
-    u.profile.address = address;
-    u.profile.education = education;
-    u.profile.work = work;
+    user.profile.religion = religion;
+    user.profile.birthday = birthday;
+    user.profile.gender = gender;
+    user.profile.relationshipStatus = "Single";
+    user.profile.languages = languages;
+    user.profile.political = political;
+    user.profile.phone = phone;
+    user.profile.address = address;
+    user.profile.education = education;
+    user.profile.work = work;
   //  u.profile.hometown = hometown;
-    u.profile.quotes = quotes;
-    u.profile.save();
-    u.save();
-    System.out.println("Relgion = " + religion);
-    renderTemplate("Application/edit_basic.html", u);
+    user.profile.quotes = quotes;
+    user.profile.save();
+    user.save();
+    renderTemplate("Application/edit_basic.html", user);
   }
 }
