8530eb7fb67973dbe2ddd23c76844bc5255a215d
Faraaz Ismail <fism88@gmail.com>
2012-04-15 01:26:10 -0500

    il8n for profile labels

--- app/controllers/Profiles.java
+++ app/controllers/Profiles.java
@@ -10,15 +10,15 @@ import models.*;
 @With(Secure.class)
 public class Profiles extends OBController {
   
-  public static void updateBasic(Long userId, String religion, Date birthday, String gender,
+  public static void updateBasic(User user, String religion, Date birthday, String gender,
       String relationshipStatus, List<UserLanguage> languages, String political, String phone,
       String address, List<Enrollment> education, List<Employment> work, Location hometown,
       String quotes) {
-    User u = User.findById(userId);
+    User u = user;
     u.profile.religion = religion;
     u.profile.birthday = birthday;
     u.profile.gender = gender;
-    u.profile.relationshipStatus = relationshipStatus;
+    u.profile.relationshipStatus = "Single";
     u.profile.languages = languages;
     u.profile.political = political;
     u.profile.phone = phone;
@@ -28,6 +28,7 @@ public class Profiles extends OBController {
   //  u.profile.hometown = hometown;
     u.profile.quotes = quotes;
     u.profile.save();
+    u.save();
     System.out.println("Relgion = " + religion);
     renderTemplate("Application/edit_basic.html", u);
   }
