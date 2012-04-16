613378f10580fb2b21cf6bd175a45382dff0d8eb
Elliot Kramer <ejameskramer@gmail.com>
2012-04-16 18:26:05 -0500

    Removed superfluous or bad annotations and includes from TempUser

--- app/models/TempUser.java
+++ app/models/TempUser.java
@@ -3,19 +3,8 @@ package models;
 import java.util.*;
 import javax.persistence.*;
 
-import org.elasticsearch.index.query.QueryBuilders;
-import controllers.Application;
-import controllers.Messages;
-import controllers.Skins;
-import controllers.Users;
-import play.db.jpa.*;
-import play.modules.elasticsearch.*;
-import play.modules.elasticsearch.annotations.ElasticSearchIgnore;
-import play.modules.elasticsearch.annotations.ElasticSearchable;
-import play.modules.elasticsearch.search.SearchResults;
 import play.libs.Crypto;
 
-@ElasticSearchable
 @Entity
 public class TempUser extends Model {
   public String first_name; // The user's first name
