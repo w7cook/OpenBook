fa5601f3f969704543e6308554b42e7dc0e0f00f
Geoffrey Parker <professional.cynic@gmail.com>
2012-04-18 20:56:15 -0500

    post creation js will no longer create empty posts.

--- public/javascripts/post.js
+++ public/javascripts/post.js
@@ -1,11 +1,13 @@
 function submitPost() {
   postData = {postContent : $("textarea#postContent").val()};
-  $.post('/posts', postData, function(data, textStatus, jqXHR) {
-    newPost = $(data).hide();
-    $('ul#post-list').prepend(newPost);
-    newPost.slideDown();
-    $('textarea#postContent').val('');
-  });
+  if (postData.postContent !== '') {
+    $.post('/posts', postData, function(data, textStatus, jqXHR) {
+      newPost = $(data).hide();
+      $('ul#post-list').prepend(newPost);
+      newPost.slideDown();
+      $('textarea#postContent').val('');
+    });
+  }
 }
 
 function deletePost(id) {
