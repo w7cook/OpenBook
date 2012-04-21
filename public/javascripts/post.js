5c4dd41f9f7bb18930772e2aefb17dd7b012b8bd
Geoffrey Parker <professional.cynic@gmail.com>
2012-04-21 15:00:32 -0500

    js for likes now good, but backend for adding a like doesn't work

--- public/javascripts/post.js
+++ public/javascripts/post.js
@@ -1,42 +1,59 @@
-function like(id, userId, like-btn, unlike-btn) {
-  alert('liking');
-  /*
+function like(id, userId, likeTxt, unlikeTxt) {
   var likes;
-  $.get("/status/${_item.id}/likes",
-        function (data, textStatus, jqXHR) {
-          likes = data;
-        },
-        "json");
-  var numlikes = likes.length();
-  
-  
+
+  $.ajax({
+    dataType: 'json',
+    async: false,
+    type: 'GET',
+    url: '/status/' + id + '/likes',
+    success: function(data, textStatus, jqXHR) {
+      likes = data;
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      alert(errorThrown);
+    }
+  });
+
+  var numlikes = likes.length;
   var bttn = $('#likeButton' + id);
   
   if(likes.indexOf('/users/' + userId) === -1) {
-    $.post("/status/${_item.id}/likes", {}, function(data) {
-      numlikes++;
-      var likeText = 'Like';
-      if (numlikes > 1)
-        likeText += 's';
-      
-      $('#numLikesPost'+id).val(numlikes + ' ' + likeText);
-      $('#likeButton' + id).val(unlike-btn);
-    });        drawLikesButton(id, like-btn);
+    $.ajax({
+      url: "/status/" + id + "/likes",
+      type: 'POST',
+      success: function(data, textStatus, jqXHR) {
+        alert('liking');
+        numlikes++;
+        var likeText = 'Like';
+        if (numlikes > 1)
+          likeText += 's';
+        
+        $('#numLikesPost'+id).val(numlikes + ' ' + likeText);
+        $('#likeButton' + id).val(unlikeTxt);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert(errorThrown);
+      }  
+    });
   }
   else {
     $.ajax({
-      url: "/status/${_item.id}/likes",
+      url: "/status/" + id + "/likes",
       type: 'DELETE',
-      'complete': function(data) {
+      success: function(data, textStatus, jqXHR) {
+        alert('unliking');
         numlikes--;
         var likeText = 'Like';
         if (numlikes > 1)
           likeText += 's';
         
         $('#numLikesPost'+id).val(numlikes + ' ' + likeText);
-        $('#likeButton' + id).val(like-btn);
+        $('#likeButton' + id).val(likeTxt);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert(errorThrown);
       }
+      
     });
   }
-  */
 }
