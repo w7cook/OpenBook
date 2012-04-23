e9671e43fe91e0b055db639304824971c179f517
Tae-sun Kim <taesun1990@gmail.com>
2012-04-22 21:32:13 -0500

    made linkedvideo inherit from post, correctly shows up in news feed

--- app/models/LinkedVideo.java
+++ app/models/LinkedVideo.java
@@ -7,7 +7,7 @@ import play.data.validation.*;
 import java.util.regex.*;
 
 @Entity
-public class LinkedVideo extends Commentable {
+public class LinkedVideo extends Post {
   
   @Required
   @URL public String link;
@@ -27,6 +27,7 @@ public class LinkedVideo extends Commentable {
   public User owner;
   
   public LinkedVideo(User owner, String title, String link, String caption, int link_type, String video_id){
+    super(owner, owner, "");
     this.title = title;
     this.owner = owner;
     this.link = link;
@@ -39,15 +40,28 @@ public class LinkedVideo extends Commentable {
   public void addCaption(String caption){
     this.caption = caption;
   }
-  
-  protected String getEmbedHtml(){
-    if(link_type == 'y')
-      return "<iframe class=\"youtube-player\" type=\"text/html\" width=\"560\" height=\"340\" src=\"http://www.youtube.com/embed/" + video_id +  "\" frameborder=\"0\"> </iframe>";
-    else if (link_type == 'd')
-      return "<iframe src=\"http://www.dailymotion.com/embed/video/" + video_id + "/\" width=\"560\" height=\"340\" frameborder=\"0\"></iframe>";
-    else if(link_type == 'v')
-      return "";
-    else
+  // 1 = small 2 = big
+  protected String getEmbedHtml(int size){
+    
+    if(size == 1){
+      if(link_type == 'y')
+        return "<iframe class=\"youtube-player\" type=\"text/html\" width=\"420\" height=\"255\" src=\"http://www.youtube.com/embed/" + video_id +  "\" frameborder=\"0\"> </iframe>";
+      else if (link_type == 'd')
+        return "<iframe src=\"http://www.dailymotion.com/embed/video/" + video_id + "/\" width=\"420\" height=\"255\" frameborder=\"0\"></iframe>";
+      else if(link_type == 'v')
+        return "";
+      else
+        return "";
+    }
+    else if(size == 2){
+      if(link_type == 'y')
+        return "<iframe class=\"youtube-player\" type=\"text/html\" width=\"560\" height=\"340\" src=\"http://www.youtube.com/embed/" + video_id +  "\" frameborder=\"0\"> </iframe>";
+      else if (link_type == 'd')
+        return "<iframe src=\"http://www.dailymotion.com/embed/video/" + video_id + "/\" width=\"560\" height=\"340\" frameborder=\"0\"></iframe>";
+      else if(link_type == 'v')
+        return "";
+      }
+      
       return "";
   }
   
