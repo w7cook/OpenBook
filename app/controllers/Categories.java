6eed0aa3af352689c68a0c825569af18842b26d2
Sean Coyle <seanpcoyle@gmail.com>
2012-04-30 00:16:38 -0500

    Completed categories.  Bootstrap work beginning on thread listing.

--- app/controllers/Categories.java
+++ app/controllers/Categories.java
@@ -12,14 +12,40 @@ import java.util.*;
 import javax.persistence.*;
 
 public class Categories extends OBController {
+  final static int MAX_CATEGORIES = 8;
+	
+	
   public static void listAll() {
     List<Category> Categories = Category.find("order by name").fetch();
-    render(Categories);
+    long nextId = 1;
+    
+    Iterator<Category> iterator = Categories.iterator();
+    
+    while (iterator.hasNext())
+    {
+    	Category cat = iterator.next();
+    	nextId = cat.id;
+    }
+    
+    
+    render(Categories, nextId);
   }
 
   public static void listSingle(Long catId) {
     Category category = Category.findById(catId);
     List<FThread> FThreads = FThread.find("category = ? order by postedAt desc", category).fetch(10);
     render(FThreads, category);
+  }
+  
+  public static void newCategory (String name, String description) {
+	if (Category.findAll().size() < MAX_CATEGORIES)
+	{
+		final Category category = new Category(name, description).save();
+	}
+  }
+  
+  public static void deleteCategory (Long categoryId) {
+	Category c = Category.findById(categoryId);
+	c.delete();
   }
 }
---
+++ public/javascripts/threads.js
@@ -0,0 +1,99 @@
+function submitCategory(id) {
+	if ($('#category-name').val() == ''
+			|| $('#category-description').val() == '') {
+		if ($('#category-name').val() == '') {
+
+		}
+		if ($('#category-description').val() == '') {
+
+		}
+	} else {
+
+		var categoryData = {
+			name : $('#category-name').val(),
+			description : $('#category-description').val()
+		};
+
+		$.ajax({
+			url : '/threads/newCategory',
+			type : 'POST',
+			data : categoryData,
+			success : function(data, textStatus, jqXHR) {
+				$('#msg-sent-alert').slideDown(300).delay(8000).slideUp(300);
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				$('#msg-failed-alert').slideDown(300).delay(8000).slideUp(300);
+			},
+			'complete' : function(jqXHR, textStatus) {
+				$('#category-name').val('');
+				$('#category-description').val('');
+			}
+		});
+
+		var newCategory = "" + "<div class='row-fluid category' id='cat-" + id
+				+ "'>" + "<div class='span8 cat-name'>"
+				+ "<h4><a href='threads/" + id + "'>"
+				+ $('#category-name').val() + "</a></h4>"
+				+ "<span class='categoryDescription'> - "
+				+ $('#category-description').val() + "</span>" + "</div>"
+				+ "<div class='span4 cat-latest cat-empty'>None</div>"
+				+ "</div>";
+
+		var $fi = $('.category');
+		var $el = $fi.eq($fi.length - 1);
+
+		$el.after(newCategory);
+
+		var newDelete = "<div id='deleteCat-" + id
+				+ "' class='deleteCat' ><a href='#' "
+				+ "onclick='deleteCategory(" + id + ")' data-dismiss='modal'>"
+				+ "<i class='icon-remove'></i></a> "
+				+ $('#category-name').val() + "</div>";
+
+		var $fi2 = $('.deleteCat');
+		var $el2 = $fi2.eq($fi2.length - 1);
+
+		$el2.after(newDelete);
+
+		var newButton = "<button class='btn btn-success' data-dismiss='modal' "
+				+ "id='createCat' onclick='submitCategory(" + ++id + ")'>"
+				+ "Create category</button>"
+
+		$('#createCat').replaceWith(newButton);
+
+		$('#numCats').html(id);
+
+		if ($('.cat-name').length >= 8) {
+			$('#newItem').remove();
+		}
+	}
+}
+
+function deleteCategory(id) {
+	var deleteData = {
+		categoryId : id,
+	};
+
+	$.ajax({
+		url : '/threads/deleteCategory',
+		type : 'POST',
+		data : deleteData,
+		success : function(data, textStatus, jqXHR) {
+			$('#msg-sent-alert').slideDown(300).delay(8000).slideUp(300);
+
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			$('#msg-failed-alert').slideDown(300).delay(8000).slideUp(300);
+		},
+		'complete' : function(jqXHR, textStatus) {
+			$('div#cat-' + id).slideUp(300);
+			$('div#deleteCat-' + id).remove();
+		}
+	});
+
+	if ($('.cat-name').length == 8) {
+		$('#delItem')
+				.before(
+						"<a class='btn btn-primary' data-toggle='modal' href='#create-category' id='newItem'>Create new category</a> ");
+	}
+}
