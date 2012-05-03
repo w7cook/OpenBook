251311c243e1c8cd9796caa8bb0b11fc47b6936c
Joseph Kiskaden <joseph.kiskaden@gmail.com>
2012-05-02 19:30:44 -0500

    rss iframe

--- app/controllers/RSSFeeds.java
+++ app/controllers/RSSFeeds.java
@@ -11,13 +11,27 @@ import models.*;
 
 @With(Secure.class)
 public class RSSFeeds extends OBController {
-  public static void addFeed(String url) {
-    User u = user();
-    RSSFeed feed = new RSSFeed(u, url);
-    // Add Unique Constraint Check
-    feed.save();
+	public static void addFeed(String url) {
+		User u = user();
+		RSSFeed feed = new RSSFeed(u, url);
+		// Add Unique Constraint Check
+		feed.save();
 
-    redirect("/");
-  }
+		redirect("/");
+	}
+	public static void RSSfeeds(Long userId) {
+		User current = User.findById(userId);
+		if (current != null){
+
+			List<RSSFeed> feeds = new ArrayList<RSSFeed>();
+
+
+			for(RSSFeed f : current.feeds) {
+				feeds.add(f);
+			}
+
+			render(feeds);
+		}
+	}
 }
 
